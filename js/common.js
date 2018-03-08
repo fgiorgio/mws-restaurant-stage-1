088317e9116edc9c3b6405c33298edd3e83ff6cc
fgiorgio <francesco.giorgio90@gmail.com>
2018-03-08 21:16:04 +0100

    Fixed semantics and service worker caching

---
+++ js/common.js
@@ -0,0 +1,6 @@
+/**
+ * Register a service worker.
+ */
+if(navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js');
+}
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -178,10 +178,3 @@ getParameterByName = (name, url) => {
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-
-/**
- * Register a service worker.
- */
-if(navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js');
-}
